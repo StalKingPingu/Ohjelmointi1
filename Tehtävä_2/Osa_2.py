b0e6c867f4af838e6cecd2bb99f8ce9ee2911af4
Luka <luka.harris04rgmail.com>
2023-08-21 12:53:34 +0300

    bruh

---
+++ Tehtävä_2/Osa_2.py
@@ -0,0 +1,5 @@
+import math
+
+säde = int(input('Anna ympyrän säde: '))
+pinta_ala = math.pi * säde ** 2
+print ("Ympyrän pinta-ala on ", pinta_ala)
---
+++ Tehtävä_2/Osa_3.py
@@ -0,0 +1,6 @@
+kanta = int(input('Anna suorakulmion kanta: '))
+korkeus = int(input('Anna suorakulmion korkeus: '))
+piiri = kanta * 2 + korkeus * 2
+pinta_ala = kanta * korkeus
+print ("Suorakulmion piiri on: ", piiri)
+print ("Ja suorakulmion pinta-ala on: ", pinta_ala)
