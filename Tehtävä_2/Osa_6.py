464eb9e1c0249619e142cfe4135b4c94f7894fa5
Luka <luka.harris04rgmail.com>
2023-08-22 13:47:59 +0300

    bruh

---
+++ Tehtävä_2/Osa_6.py
@@ -0,0 +1,15 @@
+import random
+random.seed()
+
+numero1 = (random.randint(1,9))
+numero2 = (random.randint(1,9))
+numero3 = (random.randint(1,9))
+
+numero4 = (random.randint(1,6))
+numero5 = (random.randint(1,6))
+numero6 = (random.randint(1,6))
+numero7 = (random.randint(1,6))
+
+print("Tässä 3 luvun koodi: ", numero1, numero2, numero3)
+
+print("Ja tässä 4 luvun koodi: ", numero4, numero5, numero6, numero7)
