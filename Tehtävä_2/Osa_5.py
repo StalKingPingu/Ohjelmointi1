f79a129aa8d594effdfd1db18667a23653eb7600
Luka <luka.harris04rgmail.com>
2023-08-22 14:30:23 +0300

    bruh

--- Tehtävä_2/Osa_5.py
+++ Tehtävä_2/Osa_5.py
@@ -1,3 +1,5 @@
+import math
+
 leiviskät = int(input('Kuinka monta leiviskää: '))
 naulat = int(input('Kuinka monta naulaa: '))
 luodit = int(input('Kuinka monta Luotia: '))
@@ -6,10 +8,10 @@ naulat = naulat + leiviskät * 20
 luodit = luodit + naulat * 32
 paino = luodit * 13.3
 
-kilogrammat = paino / 1000
-kilogrammat = round(kilogrammat)
+paino = float(paino)
+kilogrammat = math.floor(paino / 1000)
 kilogrammat = kilogrammat * 1000
 paino = paino - kilogrammat
 kilogrammat = kilogrammat / 1000
 
-print("Esineiden yhteismassa on: ", kilogrammat," kilogrammaa ja ", paino, " grammaa")
+print("Esineiden yhteismassa on: ", kilogrammat, " kilogrammaa ja ", paino, " grammaa")
