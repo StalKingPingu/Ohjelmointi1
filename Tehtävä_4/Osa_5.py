039d41288218736695be0a98f6906a7f59eee137
Luka <luka.harris04rgmail.com>
2023-08-29 13:54:14 +0300

    bruh

--- Tehtävä_4/Osa_5.py
+++ Tehtävä_4/Osa_5.py
@@ -1,14 +1,21 @@
 käyttäjätunnus = "python"
 salasana = "rules"
+yritykset = 0
 
 while True:
-    käyttäjätunnus2 = input('Anna käyttäjätunnus')
-    salasana2 = input('Anna salasana')
-    if käyttäjätunnus2 == käyttäjätunnus:
-        if salasana2 == salasana:
-            print("Tervetuloa!")
-            break
-        else:
+    if yritykset < 5:
+        käyttäjätunnus2 = input('Anna käyttäjätunnus')
+        salasana2 = input('Anna salasana')
+        if käyttäjätunnus2 == käyttäjätunnus:
+            if salasana2 == salasana:
+                print("Tervetuloa!")
+                break
+            else:
+                print("Pääsy evätty")
+                yritykset = yritykset + 1
+        if käyttäjätunnus2 != käyttäjätunnus:
             print("Pääsy evätty")
-    if käyttäjätunnus2 != käyttäjätunnus:
-        print("Pääsy evätty")
+            yritykset = yritykset + 1
+    else:
+        print("Yritit liian monta kertaa")
+        break
